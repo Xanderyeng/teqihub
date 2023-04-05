f2e8149914172a82b6715a5ccf29f35abb30ec71
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-04-05 12:54:01 +0300

    Updated Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,6 +5,9 @@ import { motion } from "framer-motion";
 import { staggerContainer } from "../utils/motion";
 import { navVariants } from "../utils/motion";
 import { navLinks } from "../constants/index";
+// menu items
+import { menuItems } from '../constants/menuItems';
+import MenuItems from './MenuItems';
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { logo, toggle } from "../assets";
@@ -15,7 +18,7 @@ export const Navbar = () => {
     <nav
       className={`${styles.paddingX} w-full flex items-center py-4 fixed top-0 z-20 black nav-bg`}
     >
-      <div className='w-full flex justify-between items-center max-w-screen-lg mx-auto border-2 border '>
+      <div className='w-full flex justify-between items-center max-w-screen-lg mx-auto border-0 border '>
         <div className='flex flex-row flex-nowrap justify-right items-center gap-32 '>
           <Link
             to='/'
@@ -31,16 +34,25 @@ export const Navbar = () => {
               className='w-[12em] h-auto object-contain'
             />
             {/* &nbsp; */}
-          </Link>
-
-          
+          </Link>          
         </div>
+        <div className='relative z-21 flex flex-row gap-16 border-0 border'>
+        <ul className='list-none hidden sm:flex flex-row gap-10 border-0 border'>
 
-        <div className='relative z-21 flex flex-row gap-16  border-2 border'>
-        <ul className='list-none hidden sm:flex flex-row gap-10  border-2 border'>
-            {navLinks.map((link) => (
+        {menuItems.map((menu, index) => {
+          const depthLevel = 0;
+          return (
+            <MenuItems
+              items={menu}
+              key={index}
+              depthLevel={depthLevel}
+            />
+          );
+        })}
+
+            {/* {navLinks.map((link, index) => (
               <li
-                key={link.id}
+                key={index}
                 className={`${
                   active === link.title ? "text-hover" : "text-primary"
                 } hover:text-hover text-[18px] font-medium cursor-pointer`}
@@ -48,7 +60,7 @@ export const Navbar = () => {
               >
                 <a href={`#${link.id}`}>{link.title}</a>
               </li>
-            ))}
+            ))} */}
           </ul>
           {/* <img src={toggle} className='w-32' /> */}
           <button className='gradient-mask hover:bg-blue-700 text-[16px] text-primary font-poppins font-light py-2 px-12 rounded-md'>
---
+++ src/constants/menuItems.js
@@ -0,0 +1,40 @@
+export const menuItems = [
+  // {
+  //   title: 'Home',
+  //   url: '/',
+  // },
+  {
+    title: 'Courses',
+    url: '/courses',
+    submenu: [
+      {
+        title: 'data analytics',
+        url: 'web-design',
+      },
+      {
+        title: 'data science',
+        url: 'data-science',
+      },
+      {
+        title: 'data engineering',
+        url: 'data-engineering',
+      },
+      {
+        title: 'cyber security',
+        url: 'cyber-security',
+      },
+    ],
+  },
+  {
+    title: 'Corporate',
+    url: '/corporate',
+  },
+      {
+        title: 'Partner',
+        url: 'partner',
+      },
+      // {
+      //   title: 'Our values',
+      //   url: 'our-values',
+      // },
+];
