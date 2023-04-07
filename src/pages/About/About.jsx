55704c30ee42fe8007d493a9e1fe78af4e2e2f92
Xanderyeng <50344667+Xanderyeng@users.noreply.github.com>
2023-04-07 15:04:25 +0300

    Height corrections

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -4,8 +4,8 @@ import { aboutData } from "../../constants";
 
 export const About = () => {
   return (
-    <section className='relative z-10 w-full h-auto top-[51px] border-0 border-red'>
-      <div className='relative z-0 top-0 flex flex-col items-center gap-24 py-16 border-0 border'>
+    <section className='relative z-10 w-full top-[51px] border-0 border-red'>
+      <div className='relative z-0 top-0 flex flex-col items-center gap-24 pt-16 border-0 border'>
         {/* ---------- HEADING ---------- */}
         <div className='flex flex-col gap-4 relative justify-center w-[75vw] items-center border-0 border-blue'>
           <span className='font-poppins font-semibold text-[1.8rem] flex flex-col text-white capitalize'>
